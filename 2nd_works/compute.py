171c49fbd061151941ca2b0a19e5f776d36045fc
jianglei7713 <jl7713@163.com>
2019-04-13 16:12:31 +0800

    homework
    homwork

---
+++ 2nd_works/compute.py
@@ -0,0 +1,39 @@
+def compute():
+    print('to quit the computer,please input "quit"')
+
+    first = input('please input first number:')
+    if first == 'quit':
+        exit()
+    first = int(first.strip().lstrip('0'))
+
+    OpratorList = ['+', '-', '*', '/']
+    operator = input(r'please inpute operator ,one of "+", "-", "*", "/":')
+    if operator not in OpratorList:
+        print('you can only input one of "+","-","*","/"')
+        exit()
+
+    second = input('please input second number:')
+    if second == 'quit':
+        exit()
+    second = int(second.strip().lstrip('0'))
+
+    output = 0
+    if operator == '+':
+        output = first + second
+    elif operator == '-':
+        output = first - second
+    elif operator == '*':
+        output = first * second
+    else:
+        output = first / second
+    print(output)
+
+
+while True:
+    compute()
+
+
+
+
+
+
