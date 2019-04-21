2690e23bd781a1f8ac34f02c2a99fc6fa572ec2c
jianglei7713 <jl7713@163.com>
2019-04-21 17:07:52 +0800

    Update LogRequestCount.py

--- 3rd_works/LogRequestCount.py
+++ 3rd_works/LogRequestCount.py
@@ -7,8 +7,9 @@ logs = '''
 223.88.60.88 "GET /mock/users/?t=POST HTTP/1.1" 200 
 111.30.144.7 "GET /mock/users/ HTTP/1.1" 200 
 '''
-
 l = logs.replace('"', '').split()
+
+requestlist = [l[x].upper() for x in range(1, len(l), 5)]
 
 request = set(requestlist)
 for x in request:
