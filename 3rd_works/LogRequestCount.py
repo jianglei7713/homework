ef32061936b8f189ee4003e484de9719112995fb
jianglei7713 <jl7713@163.com>
2019-04-21 15:18:49 +0800

    3rd

---
+++ 3rd_works/LogRequestCount.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python3
+
+logs = '''
+111.30.144.7 "POST /mock/login/?t=GET HTTP/1.1" 200 
+111.30.144.7 "Get /mock/users/?t=POST HTTP/1.1" 200 
+111.13.100.92 "Post /mock/login/ HTTP/1.1" 200 
+223.88.60.88 "GET /mock/users/?t=POST HTTP/1.1" 200 
+111.30.144.7 "GET /mock/users/ HTTP/1.1" 200 
+'''
+
+l = logs.replace('"', '').split()
+
+requestlist = [l[x].upper() for x in range(1, len(l), 5)]
+
+request = ['POST', 'GET']
+for x in request:
+    print(x, requestlist.count(x))
---
+++ 3rd_works/SelectionSort.py
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/env python
+
+
+l = [3, 5, 1, 7, 9, 6, 8]
+
+length = len(l)
+
+count = 0
+swap = 0
+for i in range(length):
+    max = i
+    for j in range(i + 1, length):
+        if l[max] < l[j]:
+            max = j
+            count += 1
+    if max != i:
+        l[i], l[max] = l[max], l[i]
+        swap += 1
+
+print(l)
+print(count, swap)
